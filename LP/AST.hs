1a50d1c4fb29c9b113c1395a246b8277500ac16e
Luke Palmer <Luke Palmer lrpalmer@gmail.com>
2010-11-09 20:18:23 -0700

    Added AST and Parser module.

---
+++ LP/AST.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE PatternGuards #-}
+
+module LP.AST where
+
+import qualified Data.Set as Set
+import qualified Data.Char as Char
+import qualified Data.Map as Map
+import Control.Arrow (first)
+import Data.Maybe (listToMaybe)
+import Control.Monad.Trans.State
+import Control.Monad (forever)
+import System.IO
+
+type Name = String
+
+data AST 
+    = Lambda Name AST
+    | Var Name
+    | App AST AST
+
+instance Show AST where
+    show = showAST
+
+showAST :: AST -> String
+showAST = go False False
+    where
+    go ap lp ast@(Lambda {}) = parens lp $ "\\" ++ inL ast
+    go ap lp (Var n) = n
+    go ap lp (App a b) = parens ap (go False True a ++ " " ++ go True False b)
+
+    inL (Lambda n ast) = n ++ " " ++ inL ast
+    inL x = "-> " ++ go False False x
+
+    parens True x = "(" ++ x ++ ")"
+    parens False x = x
+
+freeVars :: AST -> Set.Set Name
+freeVars (Lambda name ast) = Set.delete name (freeVars ast)
+freeVars (Var n) = Set.singleton n
+freeVars (App a b) = Set.union (freeVars a) (freeVars b)
+
+splits :: [a] -> [([a],[a])]
+splits xs = ([], xs) : go xs
+    where
+    go [] = []
+    go (x:xs) = map (first (x:)) (splits xs)
+
+find :: (a -> Bool) -> [a] -> Maybe a
+find p = listToMaybe . filter p
+
+nameExtension :: Name -> Name
+nameExtension name
+    | null name = "@0"
+    | Just (nonn, num) <- numeric = nonn ++ show (succ (read num :: Integer))
+    | last name == '\'' = name ++ "0"
+    where
+    numeric = find (all Char.isDigit . snd) (splits name)
+
+nameExtensions :: Name -> [Name]
+nameExtensions = tail . iterate nameExtension
+
+validExtension :: Set.Set Name -> Name -> Name
+validExtension set = head . filter (not . (`Set.member` set)) . nameExtensions
+
+subst :: Name -> AST -> AST -> AST
+subst name with ast = go ast
+    where
+    go (Lambda n ast)
+        | name == n = Lambda n ast
+        | n `Set.member` vars = let newName = validExtension vars n in Lambda newName (go (subst n (Var newName) ast))
+        | otherwise = Lambda n (go ast)
+    go (Var n) | name == n = with
+               | otherwise = Var n
+    go (App a b) = App (subst name with a) (subst name with b)
+
+    vars = freeVars with
+
+whnf :: AST -> AST
+whnf (App f x) = case whnf f of
+    Lambda n ast -> whnf (subst n x ast)
+    Var n -> App (Var n) x
+    App a b -> App (App a b) x
+whnf x = x
+
+normalize :: AST -> AST
+normalize (Lambda n ast) = Lambda n (normalize ast)
+normalize (App f x) = 
+    case normalize f of
+        Lambda n ast -> normalize (subst n (normalize x) ast)
+        Var n -> App (Var n) (normalize x)
+        App a b -> App (App a b) (normalize x)
+normalize (Var n) = Var n
---
+++ LP/Parser.hs
@@ -0,0 +1,37 @@
+module LP.Parser where
+
+import qualified Text.Parsec as P
+import qualified Data.Char as Char
+import LP.AST
+import Control.Applicative
+
+type Parser = P.Parsec String ()
+
+tok :: String -> Parser String
+tok = tokP . P.string
+
+tokP :: Parser a -> Parser a
+tokP = (<* P.spaces)
+
+var :: Parser AST
+var = Var <$> ident
+
+ident :: Parser Name
+ident = tokP . P.many1 . P.satisfy $ \c -> not (c `elem` " \t\n\\.")
+
+lambda :: Parser AST
+lambda = flip (foldr Lambda) <$> (tok "\\" *> P.many1 ident) <*> (tok "." *> expr)
+
+parenExpr :: Parser AST
+parenExpr = tok "(" *> expr <* tok ")"
+
+expr :: Parser AST
+expr = foldl1 App <$> P.many1 (P.choice [parenExpr, lambda, var])
+
+input :: Parser AST
+input = expr <* P.eof
+
+parse :: String -> AST
+parse s = case P.parse input "input" s of
+    Left err -> error (show err)
+    Right ast -> ast
